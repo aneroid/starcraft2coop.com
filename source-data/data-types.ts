4cc90e4bf6913b0eab677700cf652e378648b190
Anirudh Dutt <1468519+aneroid@users.noreply.github.com>
2026-04-09 12:37:10 +0530

    Human-ish ids for mutators
    MutatorId defined as `Lowercase<string>` doesn't work for the build.
      needs to be just `string`
    
    With `string`, the 2nd validation for id1 < id2 should have failed

--- source-data/data-types.ts
+++ source-data/data-types.ts
@@ -10,6 +10,10 @@ export type PositiveInteger = number;
 export type NonNegativeInteger = number;
 export type CommanderPowerRating = 1 | 2 | 3 | 4 | 5;
 export type NumericBoolean = 0 | 1;
+/**
+ * @asType string
+ */
+export type MutatorId = Lowercase<string>;
 
 export interface BrutalPlus {
     /** as in, the X in Brutal+X */
@@ -66,9 +70,9 @@ export type CommanderList = Commander[];
 
 export interface MutatorInteraction {
     /** ID of the first mutator in the pair */
-    id1: PositiveInteger;
+    id1: MutatorId;
     /** ID of the second mutator in the pair (id2 > id1) */
-    id2: PositiveInteger;
+    id2: MutatorId;
     /** Description of how the two mutators interact */
     interaction: string;
 }
