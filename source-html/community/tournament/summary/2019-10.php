b857269be25510e7b3e9a0f846bfd77664c18a70
Serine <serinemolecule@gmail.com>
2026-04-22 14:43:37 -0700

    Inline fakerows.php

--- source-html/community/tournament/summary/2019-10.php
+++ source-html/community/tournament/summary/2019-10.php
@@ -150,7 +150,9 @@ require_once "../../../../includes/wrapper-static.php";
             </tr>
             <?php
             $start = 3;
-            include("../fakerows.php");
+            for ($x = $start + 1; $x <= 5; $x++) {
+                echo '<tr class="fake round"><td class="number"><h2>Round ' . $x . '</h2></td><td class="mutset1"><div class="spoiler"><p>Click to reveal spoiler</p></div><div class="points">(x)</div><div class="muts"><div class="icons"><img class="tooltip" src="/images/tournament/blankmutator.png" alt="No Mutator" style="opacity: 1;"></div></div><div class="map"><img class="tooltip" src="/images/tournament/blankmap.png" alt="No Map"></div><div class="powers"></div><div class="links"><div class="link"><img src="/images/tournament/casticon.png" alt="Game Cast VOD"><p>No Game</p></div></div><td class="mutset2"><div class="points">(x)</div><div class="muts"><div class="icons"><img class="tooltip" src="/images/tournament/blankmutator.png" alt="No Mutator" style="opacity: 1;"></div></div><div class="map"><img class="tooltip" src="/images/tournament/blankmap.png" alt="No Map"></div><div class="powers"></div><div class="links"><div class="link"><img src="/images/tournament/casticon.png" alt="Game Cast VOD"><p>No Game</p></div></div></tr>';
+            }
             ?>
         </tbody>
 </table>
